b71b4834716eefad2b94ac18d1ff7257caac1462
Yuerino <50553280+Yuerino@users.noreply.github.com>
2022-09-15 18:53:03 +0200

    feat: get conf file path from arg (#2)

--- include/utils.hpp
+++ include/utils.hpp
@@ -6,6 +6,11 @@
 #include <fstream>
 #include <stdexcept>
 
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define RESET "\033[0m"
+
 namespace webserv {
 	/**
 	 * @brief Convert T type to string
