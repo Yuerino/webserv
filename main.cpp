524bde47950ff1f74607dee750357704646d776e
Yuerino <50553280+Yuerino@users.noreply.github.com>
2022-09-15 18:49:20 +0200

    feat: get conf file path from arg

--- main.cpp
+++ main.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
 #include <stdexcept>
+#include <stdlib.h>
+
 #include "utils.hpp"
 #include "Parser.hpp"
 
-int main() {
+int main(int argc, char **argv) {
+	if (argc != 2) {
+		std::cerr << RED << "Invalid number of arguments." << RESET << std::endl;
+		return EXIT_FAILURE;
+	}
+
 	webserv::Parser parser;
 
 	try {
-		parser.parse(webserv::file_to_string("./config/default.conf"));
+		parser.parse(webserv::file_to_string(argv[1]));
 	} catch (const std::exception& e) {
-		std::cerr << e.what() << std::endl;
+		std::cerr << RED << e.what() << RESET << std::endl;
+		return EXIT_FAILURE;
 	}
 
-	return 0;
+	return EXIT_SUCCESS;
 }
